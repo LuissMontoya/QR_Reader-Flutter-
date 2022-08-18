870224d1b4bb4ccdee39260331bf888ea90994b9
Luiss Montoya <luisgoemon@gmail.com>
2022-08-18 15:18:01 -0500

    methods

---
+++ lib/providers/scan_list_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+
+class ScanListProvider extends ChangeNotifier {
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
+
+  nuevoScan(String valor) async {
+    final nuevoScan = new ScanModel(valor: valor);
+    final id = await DBProvider.db.nuevoScan(nuevoScan);
+    //Asignar id de bd al modelo
+    nuevoScan.id = id;
+
+    if (this.tipoSeleccionado == nuevoScan.tipo) {
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+  }
+
+  cargarScans() async {
+    final scans = await DBProvider.db.getAllScan();
+    this.scans = [...?scans];
+    notifyListeners();
+  }
+
+  cargarScansTipo(String tipo) async {
+    final scans = await DBProvider.db.getScanByType(tipo);
+    this.scans = [...?scans];
+    this.tipoSeleccionado = tipo;
+    notifyListeners();
+  }
+
+  borrarTodos() async {
+    await DBProvider.db.deleteAllScans();
+    this.scans = [];
+    notifyListeners();
+  }
+
+  borrarScanId(int id) async {
+    await DBProvider.db.deleteScan(id);
+    this.cargarScansTipo(this.tipoSeleccionado);
+  }
+}
